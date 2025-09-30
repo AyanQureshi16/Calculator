a4198bc43b92e3fbf2129284b03beddf5929951b
AyanQureshi16 <ayanshahid1612@gmail.com>
2025-09-30 14:32:56 -0700

    Calculator

---
+++ app.js
@@ -0,0 +1,54 @@
+var num1 =+prompt("Addition Value 1")
+var num2 =+prompt("Addition Value 2")
+var add = num1 + num2
+
+document.writeln(
+"<table border=1px,solid>" +
+"<tr>"+
+"<th>"+"ADDITION"+"</th>"
++"</tr>"+
+"<tr>"+
+"<td>"+add+"</td>"
++"</tr>"
++"<table>"
+)
+var num1 =+prompt("Mutiplication Value 1")
+var num2 =+prompt("multiplication Value 2")
+var multiply = num1 * num2
+document.writeln(
+"<table border=1px,solid>" +
+"<tr>"+
+"<th>"+"MULTIPLICATION"+"</th>"
++"</tr>"+
+"<tr>"+
+"<td>"+multiply+"</td>"
++"</tr>"
++"<table>"
+)
+var num1 =+prompt("Subtraction Value 1")
+var num2 =+prompt("Subtraction Value 2")
+var subtract = num1 - num2
+document.writeln(
+"<table border=1px,solid>" +
+"<tr>"+
+"<th>"+"SUBTRACTION"+"</th>"
++"</tr>"+
+"<tr>"+
+"<td>"+subtract+"</td>"
++"</tr>"
++"<table>"
+)
+var num1 =+prompt("Division Value 1")
+var num2 =+prompt("Division Value 2")
+var divide = num1/num2
+document.writeln(
+"<table border=1px,solid>" +
+"<tr>"+
+"<th>"+"DIVISION"+"</th>"
++"</tr>"+
+"<tr>"+
+"<td>"+divide+"</td>"
++"</tr>"
++"<table>"
+
+)
